54940a94c32ba1938040826e7f0e7761803719e5
luohuiqi <qu%wi80g>
2021-02-07 17:07:50 +0800

    camel ftp demo add

--- src/main/java/com/zdww/camel/CamelApplication.java
+++ src/main/java/com/zdww/camel/CamelApplication.java
@@ -3,6 +3,12 @@ package com.zdww.camel;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * camel 测试 启动类
+ *
+ * @author luohuiqi
+ * @date: 2021/2/7 10:43
+ */
 @SpringBootApplication
 public class CamelApplication {
 
--- src/main/java/com/zdww/camel/route/FileRouteBuilder.java
+++ src/main/java/com/zdww/camel/route/FileRouteBuilder.java
@@ -1,23 +1,23 @@
 package com.zdww.camel.route;
 
-import com.zdww.camel.processor.FileProcessor;
 import org.apache.camel.builder.RouteBuilder;
+import org.springframework.stereotype.Component;
 
 /**
  * @author luohuiqi
  * @Description:
  * @Date: 2021/1/21 16:38
  */
+@Component
 public class FileRouteBuilder extends RouteBuilder {
 
-    private static final String SOURCE_FOLDER = "/Users/luohuiqi/work/workspace/me/camel002/src/test/java";
-    private static final String DESTINATION_FOLDER = "/Users/luohuiqi/Desktop/WW";
+    private static final String BASE_FOLDER = "/Users/luohuiqi/work/workspace/me/camel-scaffold/src/test/java/com/zdww/camel";
+    private static final String SOURCE_FOLDER = BASE_FOLDER + "/source";
+    private static final String DESTINATION_FOLDER = BASE_FOLDER + "/destination";
 
     @Override
     public void configure() throws Exception {
-        from("file://" + SOURCE_FOLDER+ "?delete=true")
-                .dynamicRouter().method(this,"dodirect")
-                .process(new FileProcessor())
-                .to("file://" + DESTINATION_FOLDER);
+        from("{{api.source}}")
+                .to("{{api.destination}}");
     }
 }
